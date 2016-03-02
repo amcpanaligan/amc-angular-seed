2d7791cc2f1bea89bad40f4fd5320e8efc870d19
unknown <apanaligan@YSPC-APANALIGAN.YouSource.Forest>
2016-03-02 18:04:56 +0800

    updated readme and initial dist file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # amc-angular-seed
 I got tired of "bowering" these 3rd party directives everytime. So I created a seed app for them. :-)
+cd C:\your\directory
+npm-install
+bower-install
+gulp-scripts
+GGWP!!
--- app/dist/main.js
+++ app/dist/main.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
     
+    // rename seed to your module name of choice. remember to edit the index.html: ng-app directive to match your module's name
     var app = angular.module('seed', [
         'angular-storage',
         'blockUI',
@@ -15,10 +16,12 @@
 })();
 
 (function () {
+    'use strict';
+    
     var app = angular.module('seed');
     
     var routeConfig = ['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-       
+        // define temporary state. ensure that routing works perfectly
         $stateProvider.state('home', {
             template: '<h1 class="text-center">Angular seed by Andrei Mari Panaligan</h1>',
             url: '/home'
